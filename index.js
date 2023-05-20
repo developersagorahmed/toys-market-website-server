026b2c2c4ceaf77a316effd2a4400461f0f29538
Sagor Ahmed <developersagorahmed@gmail.com>
2023-05-20 10:08:39 +0600

    added

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+
+		const db = client.db("toysPortal");
+		const toysCollection = db.collection("toys");
+
+		app.post("/addtoy", async (req, res) => {
+			const body = req.body;
+			console.log(body);
+		});
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log(
@@ -34,7 +42,7 @@ async function run() {
 		);
 	} finally {
 		// Ensures that the client will close when you finish/error
-		await client.close();
+		// await client.close();
 	}
 }
 run().catch(console.dir);
